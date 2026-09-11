77ad4441eabe1b4dbc4b2177b23b63677e09fb17
Krish Garg <krishuu2314@gmail.com>
2026-09-11 10:57:35 +0530

    Add files via upload

---
+++ link_list-21-08-26.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+int main() {
+    struct Node *head= NULL, *temp=NULL, *newNode=NULL;
+    int choice;
+
+    do{
+        newNode= (struct Node*)malloc(sizeof(struct Node));
+        printf("Enter data for new node: ");
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if(head==NULL){
+            head= newNode;
+            temp= newNode;
+        }
+        else{
+            temp->next= newNode;
+            temp= newNode;
+        }
+        printf("Do you want to add another node(1 for Yes/ 0 for No)");
+        scanf("%d", &choice);
+    }
+    while(choice==1);{
+        printf("\n Linked list: ");
+        temp= head;
+    }
+    do{
+        printf("%d ->", temp->data);
+        temp= temp->next;
+    }
+    while(temp!=NULL);
+    printf("NULL\n");
+
+
+    return 0;
+}
